9bb545f213613271eeb5acfd95675e688277305c
mdmehedi31 <mdmehedihasan.mh31@gmail.com>
2026-06-22 09:36:23 +0600

    init command

---
+++ src/main/java/com/dp/DockerPracticesApplication.java
@@ -0,0 +1,13 @@
+package com.dp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerPracticesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerPracticesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dp/DockerPracticesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerPracticesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
